1bbb084952798d60d4039317f6d2204ff726c644
Michael Redig <mredig@gmail.com>
2019-04-23 12:32:16 -0500

    added introduce button and linked labels to vc

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var hobbiesLabel: UILabel!
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		print("My name is and then fill in the blank with whatever your name is.")
+		
+//		print("My name is and then fill in the blank with whatever your name is.")
 	}
 
-
+	@IBAction func introduceYourselfButtonPressed(_ sender: UIButton) {
+		nameLabel.text = "Michael Redig"
+		hobbiesLabel.text = ["WoW", "Swift Coding", "3d printing"].joined(separator: ", ")
+		
+	}
+	
 }
 
